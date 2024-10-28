8839aa76c4eaa357e0a046940fa55f0174368b6f
Manjushree <manjushreec2003@gmail.com>
2024-10-29 01:03:35 +0530

    Weather-app

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/Weather-App/",
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173,
+  }
 });
+
+
